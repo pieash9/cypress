7314048e61626731386e106c2a0e236e0519c2c4
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-03-22 16:42:32 +0600

    complete landing page

---
+++ src/components/landing-page/custom-card.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps, FC, ReactNode } from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { cn } from "@/lib/utils";
+
+type CardProps = ComponentProps<typeof Card>;
+type CustomCardProps = CardProps & {
+  cardHeader?: ReactNode;
+  cardContent?: ReactNode;
+  cardFooter?: ReactNode;
+};
+
+const CustomCard: FC<CustomCardProps> = ({
+  className,
+  cardHeader,
+  cardContent,
+  cardFooter,
+  ...props
+}) => {
+  return (
+    <Card className={cn("w-[380px]", className)} {...props}>
+      <CardHeader>{cardHeader}</CardHeader>
+      <CardContent className="grid gap-4">{cardContent}</CardContent>
+      <CardFooter>{cardFooter}</CardFooter>
+    </Card>
+  );
+};
+
+export default CustomCard;
--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -2,23 +2,25 @@ import { FC } from "react";
 
 interface TitleSectionProps {
   title: string;
-  subHeading?: string;
+  subheading?: string;
   pill: string;
 }
 
-const TitleSection: FC<TitleSectionProps> = ({ title, subHeading, pill }) => {
+const TitleSection: FC<TitleSectionProps> = ({ title, subheading, pill }) => {
   return (
     <>
       <section className="flex flex-col gap-4 justify-center items-start md:items-center ">
         <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand-primaryBlue dark:to-brand-washedPurple">
           <div className="rounded-full px-3 py-1 dark:bg-black">{pill}</div>
         </article>
-        {subHeading ? (
+        {subheading ? (
           <>
             <h2 className="text-left text-3xl sm:text-5xl sm:max-w-[750px] md:text-center font-semibold">
               {title}
             </h2>
-            <p className="dark:text-washed-purple-700 sm:max-w-[450px] md:text-center"></p>
+            <p className="dark:text-washed-purple-700 sm:max-w-[450px] md:text-center">
+              {subheading}
+            </p>
           </>
         ) : (
           <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center font-semibold ">
