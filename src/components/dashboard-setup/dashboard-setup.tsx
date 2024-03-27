22774061febd7242ab0697cf099a7cc54a8fb7ad
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-03-27 22:49:53 +0600

    added workspace dashboard

---
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { AuthUser } from "@supabase/supabase-js";
+import { FC } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+interface DashboardSetupProps {
+  user: AuthUser;
+  subscription: {} | null;
+}
+
+const DashboardSetup: FC<DashboardSetupProps> = ({ subscription, user }) => {
+  return (
+    <Card className="w-[800px] h-screen sm:h-auto">
+      <CardHeader>
+        <CardTitle>Create a Workspace</CardTitle>
+        <CardDescription>
+          Lets create a private workspace to get you started.You can add
+          collaborators later from the workspace settings tab.
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <form onSubmit={() => {}}>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-4 ">
+              <div className="text-5xl"></div>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DashboardSetup;
